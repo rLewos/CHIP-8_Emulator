03ef90df79098064577abe0d3c736dd54d46db95
YUKINO-PC\Lemos <digol1023@gmail.com>
2021-11-24 11:23:52 -0300

    Initial Commit

---
+++ Chip8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Chip8;
+
+int tamanhoROM = 0;
+byte[] romArray;
+
+using (BinaryReader rom = new BinaryReader(File.Open("test_opcode.ch8", FileMode.Open)))
+{
+    tamanhoROM = Convert.ToInt32(rom.BaseStream.Length);
+    
+    romArray = new byte[tamanhoROM];
+    romArray = rom.ReadBytes(tamanhoROM);
+}
+
+CHIP8 chip = new CHIP8();
+chip.CarregarROM(romArray);
+chip.Executar();
