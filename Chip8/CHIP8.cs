7a2909df446e605b778ccb508395057007b951db
rLemos <digol1023@gmail.com>
2021-12-02 12:31:15 -0300

    (WIP) Sprites 0-F, Registers Enum, Delay and Timer Registers, and the Stack.

--- Chip8/CHIP8.cs
+++ Chip8/CHIP8.cs
@@ -12,17 +12,25 @@ namespace Chip8
         private ushort PC;
         private ushort I;
         private byte SP;
+        private byte DelayRegister;
+        private byte TimerRegister;
         private byte[] Registers;
         private byte[] Memory;
+        private byte[] Stack;
 
         public CHIP8()
         {
             this.Registers = new byte[16];
-            
+
             this.PC = 0x200;
             this.I = 0x0;
             this.Memory = new byte[4096];
             this.SP = 0x0;
+            this.DelayRegister = 0x0;
+            this.TimerRegister = 0x0;
+            this.Stack = new byte[16];
+
+            this.LoadSprites();
         }
 
         internal void Run()
@@ -31,15 +39,13 @@ namespace Chip8
             while (true)
             {
                 byte memData = this.Memory.ElementAt(this.PC);
-            
+
                 this.PC += 1;
                 byte memData2 = this.Memory.ElementAt(this.PC);
 
                 ushort opcode = (ushort)(memData << 8 | memData2);
-                Debug.WriteLine("Hex: 0x{0}", opcode);
 
-                #region Switch
-                byte instructionNumber = (byte)(opcode >> 12);                
+                byte instructionNumber = (byte)(opcode >> 12);
                 switch (instructionNumber)
                 {
                     case 0x0:
@@ -65,28 +71,28 @@ namespace Chip8
                     case 0x5:
                         this.PC += 1;
                         break;
-                    
+
                     case 0x6:
-                        registerNumber = (byte) ((opcode ^ 0x6000) >> 8);
-                        this.Registers[registerNumber] = (byte) (registerNumber << 0x8 ^ opcode ^ 0x6000);
+                        registerNumber = (byte)((opcode ^ 0x6000) >> 8);
+                        this.Registers[registerNumber] = (byte)(registerNumber << 0x8 ^ opcode ^ 0x6000);
                         this.PC += 1;
 
                         break;
-                    
+
                     case 0x7:
-                        registerNumber = (byte) ((opcode ^ 0x7000) >> 8);
-                        this.Registers[registerNumber] += (byte) (0x7000^(opcode ^ (registerNumber << 8)));
+                        registerNumber = (byte)((opcode ^ 0x7000) >> 8);
+                        this.Registers[registerNumber] += (byte)(0x7000 ^ (opcode ^ (registerNumber << 8)));
                         this.PC += 1;
 
                         break;
-                    
+
                     case 0x8:
 
                         ushort opcode_8x = (ushort)(opcode ^ 0x8000);
-                        byte mask = (byte) ((opcode_8x << 28) >> 28);
+                        byte mask = (byte)((opcode_8x << 28) >> 28);
 
-                        byte registerX = (byte) (opcode_8x >> 8);
-                        byte registerY = (byte) ((registerX << 8 ^ opcode_8x) >> 4);
+                        byte registerX = (byte)(opcode_8x >> 8);
+                        byte registerY = (byte)((registerX << 8 ^ opcode_8x) >> 4);
 
                         switch (mask)
                         {
@@ -94,57 +100,57 @@ namespace Chip8
                                 this.Registers[registerX] = this.Registers[registerY];
                                 break;
 
-                            case (0x1):
+                            case 0x1:
                                 this.Registers[registerX] |= this.Registers[registerY];
                                 break;
 
-                            case (0x2):
+                            case 0x2:
                                 this.Registers[registerX] &= this.Registers[registerY];
                                 break;
 
-                            case (0x3):
+                            case 0x3:
                                 this.Registers[registerX] ^= this.Registers[registerY];
                                 break;
 
-                            case (0x4):
+                            case 0x4:
 
                                 int x = this.Registers[registerX];
                                 int y = this.Registers[registerY];
 
                                 if (x + y > byte.MaxValue)
-                                    this.Registers[0xF] = 0x01;
+                                    this.Registers[(int)RegistersEnum.VF] = 0x01;
                                 else
-                                    this.Registers[0xF] = 0x00;
+                                    this.Registers[(int)RegistersEnum.VF] = 0x00;
 
                                 this.Registers[registerX] += this.Registers[registerY];
-                                
+
                                 break;
 
-                            case (0x5):
-                                
+                            case 0x5:
+
                                 int vx = this.Registers[registerX];
                                 int vy = this.Registers[registerY];
 
                                 if (vx - vy < byte.MinValue)
-                                    this.Registers[0xF] = 0x01;
+                                    this.Registers[(int)RegistersEnum.VF] = 0x00;
                                 else
-                                    this.Registers[0xF] = 0x00;
+                                    this.Registers[(int)RegistersEnum.VF] = 0x01;
 
                                 this.Registers[registerX] -= this.Registers[registerY];
                                 break;
 
-                            case (0x6):
+                            case 0x6:
                                 this.Registers[registerX] >>= this.Registers[registerY];
-                                this.Registers[0xF] = 0x0; // Carry
+                                this.Registers[(int)RegistersEnum.VF] = 0x0;
                                 break;
 
-                            case (0x7):
+                            case 0x7:
                                 //this.Registers[registerX] =- this.Registers[registerY];
                                 break;
 
-                            case (0xE):
+                            case 0xE:
                                 this.Registers[registerX] <<= this.Registers[registerY];
-                                this.Registers[0xF] = 0x0; // Carry
+                                this.Registers[(int)RegistersEnum.VF] = 0x0;
                                 break;
                         }
 
@@ -156,14 +162,14 @@ namespace Chip8
                         break;
 
                     case 0xA:
-                        ushort endereco = (ushort)(opcode ^ 0xa000);
+                        ushort endereco = (ushort)(opcode ^ 0xA000);
                         this.I = endereco;
                         this.PC += 1;
 
                         break;
 
                     case 0xB:
-                        this.PC += 1;
+                        this.PC = (ushort)((opcode ^ 0xB000) + this.Registers[(int) RegistersEnum.V0]);
                         break;
 
                     case 0xC:
@@ -193,11 +199,10 @@ namespace Chip8
 
                     default:
                         string error = String.Format("Instruction not implemented: {0}", opcode);
-                        
+
                         Debug.WriteLine(error);
                         throw new Exception(error);
                 }
-                #endregion
             }
         }
 
@@ -210,6 +215,15 @@ namespace Chip8
             }
 
             rom.CopyTo(this.Memory, 0x200);
+        }
+
+        private void LoadSprites()
+        {
+            ushort memoryAddressAllocation = 0x000;
+            Sprites.Zero.CopyTo(this.Memory, memoryAddressAllocation);
+            Sprites.One.CopyTo(this.Memory, memoryAddressAllocation += 0x005);
+            Sprites.Two.CopyTo(this.Memory, memoryAddressAllocation += 0x005);
+            Sprites.Three.CopyTo(this.Memory, memoryAddressAllocation += 0x005);
         }
     }
 }
---
+++ Chip8/Sprites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8
+{
+    public static class Sprites
+    {
+        public static readonly byte[] Zero =    new byte[5] { 0xF0, 0x90, 0x90, 0x90, 0xF0 };
+        public static readonly byte[] One =     new byte[5] { 0x20, 0x60, 0x20, 0x20, 0x70 };
+        public static readonly byte[] Two =     new byte[5] { 0xF0, 0x10, 0xF0, 0x80, 0xF0 };
+        public static readonly byte[] Three =   new byte[5] { 0xF0, 0x10, 0xF0, 0x10, 0xF0 };
+    }
+}
